e2008cf3ad0da20134a8dd40991e7fb8d62c7e23
Vladimir Gankin <vovgank@mail.ru>
2024-11-30 20:49:47 +0300

    исправляет ошибки по линтеру

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 const DESCRIPTIONS = [
-  "Закат над морем",
-  "Городская улица ночью",
-  "Заснеженный лес",
-  "Кафе на набережной",
-  "Горная вершина",
-  "Цветущая сакура",
-]
+  'Закат над морем',
+  'Городская улица ночью',
+  'Заснеженный лес',
+  'Кафе на набережной',
+  'Горная вершина',
+  'Цветущая сакура',
+];
 
 const MESSAGES = [
-  "Всё отлично!",
-  "В целом всё неплохо. Но не всё.",
-  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
-  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
-  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
-  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!",
-]
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
 const NAMES = [
-  "Артем",
-  "Александр",
-  "Виктор",
-  "Василий",
-  "Петр",
-  "Анастасия",
-  "Валерия",
-  "Виктория",
-  "Дарья",
-  "Елизавета",
-]
+  'Артем',
+  'Александр',
+  'Виктор',
+  'Василий',
+  'Петр',
+  'Анастасия',
+  'Валерия',
+  'Виктория',
+  'Дарья',
+  'Елизавета',
+];
 
 const getRandomInteger = function(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-}
+};
 
 const createIdGenerator = function(min, max) {
   const previousValues = [];
@@ -41,7 +41,7 @@ const createIdGenerator = function(min, max) {
     let lastGeneratedId = getRandomInteger(min, max);
 
     if(previousValues.length >= (max - min + 1)) {
-      console.log("Закончились уникальные идентификаторы");
+      console.log('Закончились уникальные идентификаторы');
       return null;
     }
 
@@ -51,8 +51,8 @@ const createIdGenerator = function(min, max) {
 
     previousValues.push(lastGeneratedId);
     return lastGeneratedId;
-  }
-}
+  };
+};
 
 const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
@@ -62,24 +62,19 @@ const generatePhotoUrlId = createIdGenerator(1, 25);
 
 const generateCommentId = createIdGenerator(1, 750);
 
-const createCommentObject = () => {
-  return {
-    id: generateCommentId(),
-    avatar:  "img/avatar-" + getRandomInteger(1, 6) + ".svg",
-    message: getRandomArrayElement(MESSAGES),
-    name: getRandomArrayElement(NAMES),
-  }
-}
+const createCommentObject = () => ({
+  id: generateCommentId(),
+  avatar:  'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
 
-const createPhotoObject = () => {
-  return {
-    id: generatePhotoId(),
-    url: "photos/" + generatePhotoUrlId() + ".jpg",
-    description: getRandomArrayElement(DESCRIPTIONS),
-    likes: getRandomInteger(15, 200),
-    comments: Array.from({length: getRandomInteger(0, 30)}, createCommentObject)
-  };
-}
+const createPhotoObject = () => ({
+  id: generatePhotoId(),
+  url: 'photos/' + generatePhotoUrlId() + '.jpg',
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: getRandomInteger(0, 30)}, createCommentObject)
+});
 
 const testPhotoObjects = Array.from({length: 25}, createPhotoObject);
-
