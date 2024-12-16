7c47e2df90f922b084f164201d738db4665cb588
Keks <keks@htmlacademy.ru>
2024-12-16 23:51:39 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,7 @@
+import { generatePhotoObjects } from "./data-generator.js";
+import "./hashtag-checker.js";
+import "./image-scaling.js";
+import "./image-effect.js";
+import "./image-upload.js";
+import "./server-upload.js"
+import "./image-filtration.js";
