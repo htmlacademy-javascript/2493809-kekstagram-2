cfb18ec5ddd01123def610b74ce870a8022911c2
Keks <keks@htmlacademy.ru>
2025-01-29 18:01:37 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import './pictures-loader.js';
+import './hashtag-checker.js';
+import './image-scaling.js';
+import './image-effect.js';
+import './image-upload.js';
+import './server-upload.js';
+import './image-filtration.js';
