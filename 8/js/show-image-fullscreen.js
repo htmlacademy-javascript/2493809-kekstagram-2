5b36680c6ae6dedad82989a24339ed8bc916bb5e
Keks <keks@htmlacademy.ru>
2025-02-23 18:12:10 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/show-image-fullscreen.js
@@ -0,0 +1,46 @@
+import { isEscapeKey } from './util.js';
+import { showComments, clearComments } from './comments-loader.js';
+
+const htmlBody = document.querySelector('body');
+const picture = document.querySelector('.big-picture');
+const picturesLikesCount = document.querySelector('.likes-count');
+const pictureCloseButton = document.querySelector('#picture-cancel');
+const pictureDescription = document.querySelector('.social__caption');
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePicturePopup();
+    document.removeEventListener('keydown', onDocumentKeydown);
+
+    clearComments();
+  }
+};
+
+function closePicturePopup() {
+  htmlBody.classList.remove('modal-open');
+
+  picture.classList.add('hidden');
+
+  clearComments();
+
+  pictureCloseButton.removeEventListener('click', closePicturePopup);
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+const openPicturePopup = (url, description, likes, comments) => {
+  htmlBody.classList.add('modal-open');
+
+  picture.classList.remove('hidden');
+  picture.querySelector('img').src = url;
+  picturesLikesCount.textContent = likes;
+  pictureDescription.textContent = description;
+  pictureCloseButton.addEventListener('click', closePicturePopup);
+
+  showComments(comments);
+
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+
+export { openPicturePopup };
