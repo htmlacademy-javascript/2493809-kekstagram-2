41f7c647093f3a43e2f1535f97cb948a7b68a535
Vladimir Gankin <vovgank@mail.ru>
2025-02-24 17:26:54 +0300

    добавляет необходимые атрибуты форме

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ import './hashtag-checker.js';
 import './image-scaling.js';
 import './image-effect.js';
 import './image-upload.js';
-import './server-upload.js';
 import './image-filtration.js';
 
 renderPictures(generatePhotoObjects());
