ff80c0e13bfc794fe9cc55e2a7331f3408d0e0c1
Vladimir Gankin <vovgank@mail.ru>
2025-03-23 00:20:31 +0300

    дорабатывает валидацию хештэгов

--- js/hashtag-validation.js
+++ js/hashtag-validation.js
@@ -1,18 +1,19 @@
 const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const validateHashtag = (value) => {
-  const hashtagArray = value.split(' ');
+  const hashtagArray = value.split(/\s+/);
+  console.log(hashtagArray);
   return hashtagArray.every((el) => hashtag.test(el));
 };
 
 const checkForRepeatingHashtag = (value) => {
-  const hashtagArray = value.toLowerCase().split(' ');
+  const hashtagArray = value.toLowerCase().split(/\s+/);
   const checkSet = new Set(hashtagArray);
   return checkSet.size === hashtagArray.length;
 };
 
 const checkForHashtagCount = (value) => {
-  const hashtagArray = value.split(' ');
+  const hashtagArray = value.split(/\s+/);
   return hashtagArray.length <= 5;
 };
 
