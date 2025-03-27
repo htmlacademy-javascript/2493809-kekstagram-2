92d1cacc5938dab80c39c3e5f599b959ceac8262
Vladimir Gankin <vovgank@mail.ru>
2025-03-27 12:58:51 +0300

    множественное число для переменной массива

--- js/hashtag-validation.js
+++ js/hashtag-validation.js
@@ -1,19 +1,19 @@
 const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const validateHashtag = (value) => {
-  const hashtagArray = value.split(/\s+/);
-  return hashtagArray.every((el) => hashtag.test(el));
+  const hashtagsArray = value.split(/\s+/);
+  return hashtagsArray.every((el) => hashtag.test(el));
 };
 
 const checkForRepeatingHashtag = (value) => {
-  const hashtagArray = value.toLowerCase().split(/\s+/);
-  const checkSet = new Set(hashtagArray);
-  return checkSet.size === hashtagArray.length;
+  const hashtagsArray = value.toLowerCase().split(/\s+/);
+  const checkSet = new Set(hashtagsArray);
+  return checkSet.size === hashtagsArray.length;
 };
 
 const checkForHashtagCount = (value) => {
-  const hashtagArray = value.split(/\s+/);
-  return hashtagArray.length <= 5;
+  const hashtagsArray = value.split(/\s+/);
+  return hashtagsArray.length <= 5;
 };
 
 const validateAllHashtags = (value) => {
